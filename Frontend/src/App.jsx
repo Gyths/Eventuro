1aa450490603a7dd9dd123e993fbb3102b956e49
KevinDM17 <kdiazmendieta17@gmail.com>
2025-09-30 01:58:51 -0500

    Login y Registro

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
 import CrearTicketTajeta from "./components/CrearTicketTajeta";
 import UbicacionEvento from "./components/UbicacionEvento";
+import BotonCTA from "./components/BotonCTA";
+import BotonEliminar from "./components/BotonEliminar";
+import LoginCard from "./components/LoginCard";
+import RegistroCard from "./components/RegistroCard";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="w-full flex items-center justify-center p-2">
-        <UbicacionEvento />
-      </div>
-      <div className="w-full flex items-start justify-center p-2">
-        <CrearTicketTajeta />
-      </div>
-    </>
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <RegistroCard/>
+    </div>
   );
 }
 
