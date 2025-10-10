207ffaad0e0f5958aaa615a40b830adc262af76c
Adrián Arista <a20201550@pucp.edu.pe>
2025-10-10 02:54:19 -0500

    Integración de generación de ordenes y tickets

--- Frontend/src/components/payment/tests.jsx
+++ Frontend/src/components/payment/tests.jsx
@@ -8,53 +8,6 @@ export function select_test(testNum, eventId) {
         quantity: 1,
       },
     ],
-    [
-      {
-        eventId: eventId,
-        eventDateId: 3,
-        eventDateZoneId: 5,
-        quantity: 1,
-        seatId: 241,
-      },
-    ],
-    [
-      {
-        eventId: eventId,
-        eventDateId: 2,
-        eventDateZoneId: 3,
-        eventDateZoneAllocationId: 4,
-        quantity: 1,
-        seatId: 121,
-      },
-    ],
-    [
-      {
-        eventId: eventId,
-        eventDateId: 2,
-        eventDateZoneId: 3,
-        eventDateZoneAllocationId: 5,
-        quantity: 1,
-        seatId: 123,
-      },
-    ],
-    [
-      {
-        eventId: eventId,
-        eventDateId: 2,
-        eventDateZoneId: 3,
-        eventDateZoneAllocationId: 4,
-        quantity: 1,
-        seatId: 124,
-      },
-      {
-        eventId: eventId,
-        eventDateId: 2,
-        eventDateZoneId: 3,
-        eventDateZoneAllocationId: 1,
-        quantity: 1,
-        seatId: 125,
-      },
-    ],
   ];
 
   return tests[testNum - 1];
