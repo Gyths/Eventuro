be6c6595a30968a8b9a74dfcb1d1210e253eb22a
Adrián Arista <a20201550@pucp.edu.pe>
2025-10-12 15:45:19 -0500

    endpoint para consultar la disponibilidad de un evento

--- Backend/src/controllers/event.controller.js
+++ Backend/src/controllers/event.controller.js
@@ -1,4 +1,8 @@
-import { createEventSvc, listEventSvc } from "../services/event.service.js";
+import {
+  createEventSvc,
+  listEventSvc,
+  listAvailableTicketsSvc,
+} from "../services/event.service.js";
 import { toJSONSafe } from "../utils/serialize.js";
 
 export async function createEvent(req, res) {
@@ -14,6 +18,15 @@ export async function listEvent(req, res) {
   try {
     const users = await listEventSvc();
     return res.status(200).json(toJSONSafe(users));
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+}
+
+export async function listAvailableTickets(req, res) {
+  try {
+    const availableTickets = await listAvailableTicketsSvc(req.body);
+    return res.status(201).json(toJSONSafe(availableTickets));
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
