14181de223498286861ca29c358dfa80bccbc57e
DiegoCCH <diego171101@gmail.com>
2025-11-10 14:08:40 -0500

    correccion, se agregaron imports

--- Backend/src/routes/user2.routes.js
+++ Backend/src/routes/user2.routes.js
@@ -3,8 +3,10 @@ import { verifyToken } from '../middlewares/ensureAuth.js';
 import { attachUserContext } from '../middlewares/ensureAuth.js';
 import { requireAdmin } from '../middlewares/ensureAuth.js';
 import { findUserByIdFull } from '../controllers/user.controller.js';
-import { getMe, putMe,changeMyPassword } from '../controllers/user.controller.js';
+import { getMe, putMe, changeMyPassword } from '../controllers/user.controller.js';
 import { ensureAuth } from '../middlewares/ensureAuth.js';
+import { updateUserStatus } from '../controllers/user.controller.js';
+import { listUsers } from '../controllers/user.controller.js';
 const router = Router();
 
 router.get('/:id', verifyToken, attachUserContext, requireAdmin, findUserByIdFull);
