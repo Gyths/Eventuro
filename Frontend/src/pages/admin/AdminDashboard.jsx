2210f72a7da62bb7191589a3aeefac0e99170bc1
bigmacs2 <166459728+bigmacs2@users.noreply.github.com>
2025-11-10 09:39:34 -0500

    Update AdminDashboard.jsx

--- Frontend/src/pages/admin/AdminDashboard.jsx
+++ Frontend/src/pages/admin/AdminDashboard.jsx
@@ -416,7 +416,7 @@ export default function AdminDashboard() {
           {/* Fila 1: Gráfico de Ingresos */}
           <IncomeChartCard />
 
-          {/* Próximos Gráficos (Barra y Pastel) irán aquí */}
+          {/* Próximos Gráficos (Barra y Pastel) */}
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <div className="lg:col-span-2 bg-white shadow-lg rounded-xl border border-gray-200 h-96 p-5 flex items-center justify-center text-gray-400">
               (Próximamente: Gráfico de Barras)
